4eee04dd6ec11efa6822b1c57a52c81124614e1a
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2024-11-16 23:34:23 +0900

    Fix: 환경 변수 불러오기 수정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,19 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.env`,
-    }),
-    AuthModule,
-    UsersModule,
-    PrismaModule,
-  ],
+  imports: [AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
