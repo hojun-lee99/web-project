573eb20abc3ff49603a0e45a113d1734109fedcf
magik99 <hojun0810@naver.com>
2024-11-17 19:33:39 +0900

    Feat: register dto 작성

---
+++ server/src/auth/dto/register.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(20)
+  nickname: string;
+}
