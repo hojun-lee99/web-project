9f5cb33ab523df202354b0c886ceae946426aae3
magik99 <hojun0810@naver.com>
2024-11-18 14:39:45 +0900

    Feat: updateArticle dto 수정

--- server/src/articles/dto/updateArticle.dto.ts
+++ server/src/articles/dto/updateArticle.dto.ts
@@ -1,5 +1,12 @@
 export class UpdateArticleDto {
+  id: number;
   title?: string;
   content?: string;
   access_toekn: string;
+}
+
+export class updateArticle {
+  id: number;
+  title?: string;
+  content?: string;
 }
