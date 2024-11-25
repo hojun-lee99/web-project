033734c2f09ad8f7f2ed49115245d174e948e792
magik99 <hojun0810@naver.com>
2024-11-25 16:27:21 +0900

    Init: reatings 초기 파일 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommunityModule } from './community/community.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, CommunityModule],
+  imports: [AuthModule, UsersModule, PrismaModule, CommunityModule, RatingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
