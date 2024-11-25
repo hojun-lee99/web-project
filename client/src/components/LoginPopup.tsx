40647c2de82fd36d696e246183d22e11aa6dcf8a
dpwl35 <dpwl9435@gmail.com>
2024-11-25 22:11:19 +0900

    feat: login 팝업 아이콘 추가

--- client/src/components/LoginPopup.tsx
+++ client/src/components/LoginPopup.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import Image from 'next/image';
 
 interface LoginPopupProps {
   type: 'login' | 'signup';
@@ -78,16 +79,36 @@ export default function LoginPopup({
 
           <SocialLogin>
             <button type="button" className="btn-naver">
-              네이버
+              <Image
+                src="/icon-naver.png"
+                alt="네이버 로고"
+                width={50}
+                height={50}
+              />
             </button>
             <button type="button" className="btn-github">
-              깃허브
+              <Image
+                src="/icon-github.svg"
+                alt="깃허브 로고"
+                width={50}
+                height={50}
+              />
             </button>
             <button type="button" className="btn-kakao">
-              카카오
+              <Image
+                src="/icon-kakao.svg"
+                alt="카카오 로고"
+                width={30}
+                height={30}
+              />
             </button>
             <button type="button" className="btn-google">
-              구글
+              <Image
+                src="/icon-google.svg"
+                alt="구글 로고"
+                width={30}
+                height={30}
+              />
             </button>
           </SocialLogin>
         </Login>
@@ -168,13 +189,38 @@ const Login = styled.div`
       background: none;
       border: none;
       margin-left: 10px;
+      cursor: pointer;
     }
   }
+
   hr {
-    background-color: var(--color-border-primary);
-    height: 1px;
+    position: relative;
+    height: 1.5rem;
     border: none;
-    margin: 20px 0;
+    margin: 30px 0;
+    width: 100%;
+
+    &::after {
+      content: 'OR';
+      color: var(--color-text-secondary);
+      vertical-align: middle;
+      background-color: var(--color-background-primary);
+      padding: 0 14px;
+      display: inline-block;
+      position: relative;
+      top: 0;
+      left: 50%;
+      transform: translate(-50%);
+      font-size: 14px;
+    }
+
+    &::before {
+      content: '';
+      border-top: 1px solid var(--color-border-primary);
+      width: 100%;
+      position: absolute;
+      top: 50%;
+    }
   }
 `;
 
@@ -189,5 +235,10 @@ const SocialLogin = styled.div`
     border: 1px solid var(--color-border-primary);
     background: none;
     border-radius: 50%;
+    cursor: pointer;
+  }
+
+  .btn-kakao {
+    background-color: #f7e317;
   }
 `;
