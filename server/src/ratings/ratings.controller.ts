5a1c69ddd4376b310fd1db987230da1278a647a7
magik99 <hojun0810@naver.com>
2024-11-28 14:53:53 +0900

    Feat: 별점 기능구현

--- server/src/ratings/ratings.controller.ts
+++ server/src/ratings/ratings.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { RatingsService } from './ratings.service';
+import { CreateRatingDto, UpdateRatingDto } from './dto';
+import { ContentType } from '@prisma/client';
 
 @Controller('ratings')
 export class RatingsController {
   constructor(private readonly ratingsService: RatingsService) {}
+
+  @Post()
+  create(@Request() req, @Body() createRatingDto: CreateRatingDto) {
+    return this.ratingsService.create({
+      userId: req.user.id,
+      ...createRatingDto,
+    });
+  }
+
+  @Get()
+  findAll(@Request() req) {
+    return this.ratingsService.findAll(req.user.id);
+  }
+
+  @Get()
+  findOne(@Param('id') id: string) {
+    return this.ratingsService.findOne(+id);
+  }
+
+  @Get('content/:contentId/:contentType')
+  findByContent(
+    @Param('contentId') contentId: string,
+    @Param('contentType') contentType: ContentType,
+  ) {
+    return this.ratingsService.findByContent(contentId, contentType);
+  }
+
+  @Patch(':id')
+  update(
+    @Request() req,
+    @Param('id') id: string,
+    @Body() updateRatingDto: UpdateRatingDto,
+  ) {
+    return this.ratingsService.update({
+      userId: req.user.id,
+      id: +id,
+      ...updateRatingDto,
+    });
+  }
+
+  @Delete(':id')
+  remove(@Request() req, @Param('id') id: string) {
+    return this.ratingsService.remove(req.user.id, +id);
+  }
 }
