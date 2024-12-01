e94cc453931cbd34b2f2813ede7d43e27227d1a7
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2024-12-01 15:41:58 +0900

    Fix: DB MYSQL => Postgresql 이전

--- server/prisma/migrations/20241120104307_add_category_id_to_article/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `categoryId` to the `articles` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `articles` ADD COLUMN `categoryId` INTEGER NOT NULL;
