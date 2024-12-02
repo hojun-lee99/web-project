47501819408024a8d6c2791b7d26308e93f566f3
dpwl35 <dpwl9435@gmail.com>
2024-12-02 23:17:48 +0900

    feat: 에러 수정

--- client/src/components/elements/TitleLg.tsx
+++ client/src/components/elements/TitleLg.tsx
@@ -2,14 +2,16 @@
 
 import styled from 'styled-components';
 
-export default function TitleLg({ title }) {
-  return (
-    <PageTitleLg>{title}</PageTitleLg>
-  );
+type TitleLgProps = {
+  title: string; // title은 문자열 타입이어야 함
+};
+
+export default function TitleLg({ title }: TitleLgProps) {
+  return <PageTitleLg>{title}</PageTitleLg>;
 }
 
 const PageTitleLg = styled.h2`
-  font-size:42px;
-  padding:12px 0 14px;
-  line-height:51px;
+  font-size: 42px;
+  padding: 12px 0 14px;
+  line-height: 51px;
 `;
