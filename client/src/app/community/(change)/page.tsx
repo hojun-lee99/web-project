4eb293f7a8c91d9969fc6e6cd50402b70fe364c5
chamsaegugu9 <rkdehdtjd123@naver.com>
2025-02-25 01:10:22 +0900

    Feat:/community 경로 구조 변경

--- client/src/app/community/(change)/page.tsx
+++ client/src/app/community/(change)/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import styled from 'styled-components';
-import PostList from '../../components/PostList';
+import PostList from '@/components/PostList';
 
 const fakePost = [
   {
