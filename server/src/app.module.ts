62f6d1fbd5b453c5e4b73b79066441e145ff8f42
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-29 10:15:56 +0900

    chore: validator tranformer 추가 및 전역 파이프 모듈 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { PipeModule } from './common/pipe/pipe.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PipeModule,
     PrismaModule,
     AuthModule,
     UsersModule,
