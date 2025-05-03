26ee7b1df15088254ee9321cfe7d0551173f1033
magik99 <hojun0810@naver.com>
2025-05-03 14:36:19 +0900

    feat: 리뷰삭제 기능 구현

--- server/src/reviews/reviews.controller.ts
+++ server/src/reviews/reviews.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
+import { AccessTokenGuard } from 'src/common/guard/access-token.guard';
+import { CurrnetUser } from 'src/common/decorator/current-user.decorator';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':reviewId')
+  async deleteReview(
+    @Param('reviewId') reviewId: string,
+    @CurrnetUser() userId: string,
+  ) {
+    await this.reviewsService.deleteReview(userId, reviewId);
+  }
 }
--- server/src/reviews/reviews.service.ts
+++ server/src/reviews/reviews.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ReviewsRepository } from './reviews.repository';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(private readonly reviewsRepository: ReviewsRepository) {}
+
+  async deleteReview(userId: string, reviewId: string) {
+    const review = await this.reviewsRepository.findById(reviewId);
+
+    if (!review) {
+      throw new NotFoundException('리뷰를 찾을 수 없습니다.');
+    }
+    if (review.userId !== userId) {
+      throw new ConflictException('리뷰 작성자가 아닙니다.');
+    }
+
+    await this.reviewsRepository.deleteReview(reviewId);
+  }
+}
